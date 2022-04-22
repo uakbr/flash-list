edc6cd21c65be8d510bba1d02f3814ab8eccfedf
Marek Fořt <marek.fort@shopify.com>
2022-04-22 17:42:25 +0200

    WIP: Successfully migrate CellContainer

--- ios/Sources/CellContainerComponentView.h
+++ ios/Sources/CellContainerComponentView.h
@@ -11,12 +11,15 @@
 //#ifdef RN_FABRIC_ENABLED
 
 #import <UIKit/UIKit.h>
+#ifdef __cplusplus
 
 #import <React/RCTViewComponentView.h>
 
 @interface CellContainerComponentView : RCTViewComponentView
 
 @end
+
+#endif
 
 //#endif // RN_FABRIC_ENABLED
 
--- src/CellContainer.ts
+++ src/CellContainer.ts
@@ -2,7 +2,7 @@
 import { requireNativeComponent } from "react-native";
 
 const CellContainer = (global as any)?.nativeFabricUIManager
-  ? require("fabric/CellContainer").default
+  ? require("fabric/CellContainerNativeComponent").default
   : requireNativeComponent("CellContainer");
 
 export default CellContainer;
